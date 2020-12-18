e08ed84694a025274ba0101c49aefce12ec78a68
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-18 21:03:54 +0400

    update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,42 @@ using namespace std;
 #define buf_size 1024
 
 int main() {
-	cout << "Hello World!";
+
+	HANDLE pipe1;
+	char buffer[buf_size];
+	DWORD num_byte;
+
+	pipe1 = CreateFile(
+		"\\\\.\\pipe\\pipe1", 
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		0,
+		NULL
+	);
+
+	cin >> buffer;
+
+	BOOL write1 = WriteFile(
+		pipe1,
+		buffer,
+		sizeof(buffer),
+		&num_byte,
+		NULL
+	);
+
+	BOOL read1 = ReadFile(
+		pipe1,
+		buffer,
+		sizeof(buffer),
+		&num_byte,
+		NULL
+	);
+
+	cout << "Message: " << buffer << endl;
+
+	CloseHandle(pipe1);
 
 	return 0;
 }
